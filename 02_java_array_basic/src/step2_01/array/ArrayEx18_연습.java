eaf90be23e6de54bb88c64ff2539244547f8f40b
sanghui <tkdgml719@gamil.com>
2023-12-21 11:51:39 +0900

    2023-12-21 예제18번 업데이트

--- 02_java_array_basic/src/step2_01/array/ArrayEx18_연습.java
+++ 02_java_array_basic/src/step2_01/array/ArrayEx18_연습.java
@@ -1,5 +1,7 @@
 package step2_01.array;
 
+//2023-12-20
+import java.util.Random;
 import java.util.Scanner;
 
 /*
@@ -30,28 +32,79 @@ import java.util.Scanner;
  *
  */
 
-
 public class ArrayEx18_연습 {
-	
-public static void main(String[] args) {
-		
+
+	public static void main(String[] args) {
+
 		Scanner scan = new Scanner(System.in);
-		
-		int[] game = {1, 2, 3, 4, 5, 6, 7, 8};
-		int[] p1   = {0, 0, 0, 0, 0, 0, 0, 0};
-		int[] p2   = {0, 0, 0, 0, 0, 0, 0, 0};
-		
-		int turn = 0;
-		
-		int idx1 = 0;	
+		Random ran = new Random();
+
+		int[] game = { 1, 2, 3, 4, 5, 6, 7, 8 };
+		int[] p1 = { 0, 0, 0, 0, 0, 0, 0, 0 };
+		int[] p2 = { 0, 0, 0, 0, 0, 0, 0, 0 };
+
+		int turn = 0;// 짝수일때 p1, 홀수일때 p2
+
+		int idx1 = 0;
 		int idx2 = 0;
-		
-		p1[idx1] = 1;	
+
+		p1[idx1] = 1;
 		p2[idx2] = 2;
-		
-		int win1 = 0;	
-		int win2 = 0;					
-		
+
+		// 먼저 3바퀴가 되면 이긴다
+		int win1 = 0;
+		int win2 = 0;
+
+		while (true) {
+			// 게임 루프
+
+			for (int i = 0; i < game.length; i++) {
+				System.out.print(game[i] + " ");
+			}
+			System.out.println();
+			for (int i = 0; i < p1.length; i++) {
+				System.out.print(p1[i] + " ");
+			}
+			System.out.println("[" + win1 + "]바퀴");
+
+			for (int i = 0; i < p2.length; i++) {
+				System.out.print(p2[i] + " ");
+			}
+			System.out.println("[" + win2 + "]바퀴");
+			if (turn % 2 == 0) {
+				System.out.print("[p1사용자]1~3 :입력  ");
+				int moveIdx = scan.nextInt();
+				// 현재 위치 초기화 및 이동 후 위치 설정
+				p1[idx1] = 0;
+				idx1 = idx1 + moveIdx;
+
+				// 바퀴를 한 바퀴 다 돌았을 경우 win의 갯수 증가
+				if (idx1 >= 8) { // 행 나누기 와 같은 문맥..?
+					win1++;
+				}
+				// 인덱스를 8로 나눈 나머지를 설정하여 원형 구조로 유지
+				idx1 = idx1 % 8;//
+
+				p1[idx1] = 1;
+
+			} else if (turn % 2 == 1) {
+				System.out.println("[p1사용자]1~3 :입력  ");
+				int moveIdx2 = ran.nextInt(3) + 1;
+
+				p2[idx2] = 0;
+				idx2 = idx2 + moveIdx2;
+
+				if (idx2 > 7) {
+					win2++;
+				}
+				idx2 = idx2 % 8;
+
+				p2[idx2] = 1;
+
+			}
+
+		}
+
 	}
 
 }
