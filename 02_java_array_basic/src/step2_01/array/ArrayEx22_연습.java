d2dc6e32ed17c22b4a9b958879698aad79e26906
sanghui <tkdgml719@gamil.com>
2023-12-31 02:11:19 +0900

    2023-12-31 연습커밋

--- 02_java_array_basic/src/step2_01/array/ArrayEx22_연습.java
+++ 02_java_array_basic/src/step2_01/array/ArrayEx22_연습.java
@@ -1,6 +1,9 @@
 package step2_01.array;
 //2023-12-27 
+import java.util.Arrays;
 import java.util.Scanner;
+
+import javax.lang.model.element.Element;
 
 
 /*
@@ -23,11 +26,11 @@ public class ArrayEx22_연습 {
 		Scanner scan = new Scanner(System.in);
 		
 		int[] arr = {10 , 20 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0};
-		int elementCnt = 2;
+		int elementCnt = 2;// 현재 배열에 존재하는 원소의 갯수
 		int selectMenu = 0;
 		
 		while (true) {
-			
+			System.out.println(Arrays.toString(arr));
 			System.out.println("\n1추가");
 			System.out.println("2삽입");
 			System.out.println("3삭제");
@@ -42,24 +45,66 @@ public class ArrayEx22_연습 {
 					System.out.println("배열의 크기를 넘어갑니다!");
 					continue;
 				}
-				System.out.print("추가할 위치 입력 : ");
+				
+				System.out.print("추가할 데이터 입력 : ");
 				int addData = scan.nextInt();
 				
+				//배열의 끝에 데이터 추가
 				arr[elementCnt] = addData;
 				elementCnt++;
 				
 			}else if (selectMenu == 2) {
 				
-				int idx = -1;
+				if (elementCnt > arr.length) {
+					System.out.println("배열의 길이를 넘어갑니다!");
+					continue;
+				}
+				System.out.print("삽입할 위치 입력 : ");
+				int idx = scan.nextInt();
+				
+				
+				//삽입할 위치의 범위 정해주기
+				if (idx < 0 || idx >= arr.length) {
+					System.out.println("해당 위치에 삽입할 수 없습니다!\n");
+					continue;
+				}
+				
+				System.out.print("삽입할 데이터 입력 : ");
+				int insertData = scan.nextInt();
+				
+				
+				for (int i = elementCnt; i < idx; i--) {// 배열에 존재하는 원소의 개수에서 삽입할 위치까지 
+					arr[i] = arr[i-1];// 한칸식 오른쪽으로 빼기					
+				}
+				arr[idx] = insertData;
+				elementCnt++;
+
+				idx = -1;
 				 System.out.print("삽입할 위치를 입력하세요 : ");
 				 int myIdx = scan.nextInt();
 				 System.out.print("삽입할 데이터를 입력하세요 : ");
 				 int myData = scan.nextInt();
-				 
-				
-				 
-				
 			}else if (selectMenu == 3) {
+				//삭제하기
+				System.out.print("삭제할 값 입력 : ");
+				int delData = scan.nextInt();
+				
+				int delIdx = -1;
+				for (int i = 0; i < elementCnt; i++) {
+					if (arr[i] == delData) {
+						delIdx = i;
+					}
+				}
+				if (delIdx == -1) {
+					System.out.println("삭제할 값이 없습니다.");
+					
+				}else {
+					for (int i = delIdx; i < elementCnt-1; i++) {
+						arr[i]=arr[i+1];
+					}
+				}
+				arr[elementCnt-1] = 0;
+				elementCnt--;
 				
 			}else if(selectMenu == 4) {
 				break;
--- 02_java_array_basic/src/step2_01/array/ArrayEx23_연습.java
+++ 02_java_array_basic/src/step2_01/array/ArrayEx23_연습.java
@@ -1,5 +1,7 @@
 package step2_01.array;
-
+import java.util.Arrays;
+import java.util.Iterator;
+//2023-12-29
 import java.util.Scanner;
 
 /*
@@ -26,17 +28,95 @@ public class ArrayEx23_연습 {
 		int selectMenu = 0;
 		boolean isRun = true;
 		
+		
 		while (isRun) {
+			
+			System.out.println(Arrays.toString(accs));
+			System.out.println(Arrays.toString(pws));
 			
 			System.out.println("1.가입");
 			System.out.println("2.탈퇴");
 			System.out.println("3.종료");
 			System.out.print("메뉴 선택 : ");
 			selectMenu = scan.nextInt();
-			
-			if		(selectMenu == 1) {}
-			else if (selectMenu == 2) {}
-			else if (selectMenu == 3) {}
+
+			if (selectMenu == 1) {
+
+				if (accsCnt >= accs.length) {
+					System.out.println("[경고] 더이상 계좌를 만들 수 없습니다!");
+				}
+
+				System.out.print("계좌 번호를 입력하세요 : ");
+				int myAccs = scan.nextInt();
+
+				// 중복여부를 boolean값으로 설정
+				boolean checkIdx = false;
+				for (int i = 0; i < accsCnt; i++) {
+					if (myAccs == accs[i]) {
+						checkIdx = true;
+						break;
+					}
+				}
+
+				if (checkIdx) {
+					System.out.println("[경고] 아이디가 중복됩니다!");
+				}
+
+				else {
+					System.out.print("패스워드를 입력하세요 : ");
+					int myPw = scan.nextInt();
+
+					accs[accsCnt] = myAccs;
+					pws[accsCnt] = myPw;
+
+					for (int i = 0; i < accsCnt; i++) {
+						if (myAccs != accs[i] && myPw != pws[i]) {// 기존배열에 없는 id,pw 들어와야지만
+							System.out.println("가입 완료!\n");// 가입 완료
+							break;
+						} else {
+							System.out.println("아이디와 패스워드를 재확인 해주세요");
+						}
+					}
+					accsCnt++;
+				}
+			}
+			else if (selectMenu == 2) {
+
+
+				System.out.print("탈퇴할 계좌번호를 입력하세요 : ");
+				int myAccNumber = scan.nextInt();
+
+
+				//중복확인을 하여 중복되면 해당 계좌번호 보유 -> 탈퇴 가능
+				int delIdx = -1;
+				for (int i = 0; i < accsCnt; i++) {
+					if (myAccNumber == accs[i]){
+						delIdx = i;
+						break;
+					}
+				}
+				if (delIdx == -1){
+					System.out.println("[경고] 아이디를 재확인 해주세요!");
+				}
+				else { // 아이디를 보유하고 있다면
+					System.out.print("계좌 번호를 입력하세요 : ");
+					int myPw = scan.nextInt();
+					
+					for (int i = delIdx; i < accsCnt-1; i++) {
+						accs[i] = accs[i+1];
+						pws[i] = accs[i+1];
+					}
+					accsCnt--;
+					System.out.println("탈퇴 완료!\n");
+					accs[accsCnt]=0;
+					pws[accsCnt] =0;
+				}
+			}
+			else if (selectMenu == 3) {
+				System.out.println("==시스템 종료==");
+				scan.close();
+				break;
+			}
 			
 		}
 
