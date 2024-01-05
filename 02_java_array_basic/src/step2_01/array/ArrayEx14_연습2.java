717d3c0922901f48499ded99837e754f8817d03a
sanghui <tkdgml719@gamil.com>
2024-01-05 14:34:38 +0900

    2024-01-05 오전연습- 14번문제 업데이트 필요

--- 02_java_array_basic/src/step2_01/array/ArrayEx14_연습2.java
+++ 02_java_array_basic/src/step2_01/array/ArrayEx14_연습2.java
@@ -1,5 +1,16 @@
 package step2_01.array;
+//2023-01-05 다시 풀기
+
+import java.util.Scanner;
 
 public class ArrayEx14_연습2 {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		int[] game = {0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+		int playerIdx = 0;
+		
+		
+	}
 
 }
